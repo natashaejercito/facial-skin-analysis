037624a263cb0f4c5470dcc3f76e64d6fcd382b9
Natasha Nicole Ejercito <natasha.ejercito@outlook.com>
2026-01-20 14:26:05 -0500

    tested client and server routes

--- server/server.js
+++ server/server.js
@@ -3,3 +3,11 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Backend working!");
+});
+app.listen(HTTP_PORT, () => {
+  console.log(`server is running on: http://localhost:${HTTP_PORT}`);
+});
